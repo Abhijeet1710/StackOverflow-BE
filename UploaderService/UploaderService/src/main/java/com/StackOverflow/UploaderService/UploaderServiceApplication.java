8cdb63f7dce84859587cda2758cd2a0013b2c564
Abhijeet1710 <abhijeetkhamkar30@gmail.com>
2025-05-25 12:33:41 +0530

    added discovery service and workign

--- UploaderService/UploaderService/src/main/java/com/StackOverflow/UploaderService/UploaderServiceApplication.java
+++ UploaderService/UploaderService/src/main/java/com/StackOverflow/UploaderService/UploaderServiceApplication.java
@@ -2,8 +2,10 @@ package com.StackOverflow.UploaderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UploaderServiceApplication {
 
 	public static void main(String[] args) {
